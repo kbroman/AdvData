e419deed1097419ca640acfe00fecc819b432617
Karl Broman <kbroman@gmail.com>
2020-02-03 09:21:30 -0600

    Lec 5: Add slides re problems with simple method and response vs no. cells

--- 05_tcells/R/plot_mean_response.R
+++ 05_tcells/R/plot_mean_response.R
@@ -1,4 +1,5 @@
 library(npem)
+library(broman) # for grayplot()
 data(p713)
 
 first <- cumsum(c(1, p713$n))[1:4]
@@ -12,10 +13,12 @@ for(i in seq_along(first)) {
 }
 
 
+pdf("../Figs/mean_response.pdf", width=10, height=5.5)
 par(mfrow=c(2,2), mar=c(4.1,3.6,2.1,0.6))
 for(i in 1:4) {
     grayplot(response[,1], response[,i+1], ylim=c(0, ifelse(i>1,4500,500)), yaxs="i",
              xlab="No. cells", ylab="Average response",
-             main=c("Cells only", "gB", "gD", "Tetox")[i]
+             main=c("Cells only", "gD2", "gB2", "Tetox")[i]
              )
 }
+dev.off()
