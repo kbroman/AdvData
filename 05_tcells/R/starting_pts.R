803465b3bdca3a1fdeb697cdfcfe6d758c0a4c37
Karl Broman <kbroman@gmail.com>
2020-02-03 10:56:32 -0600

    Lec 5: Add slides with multiple starting points and multiple modes

--- 05_tcells/R/starting_pts.R
+++ 05_tcells/R/starting_pts.R
@@ -1,4 +1,5 @@
 library(npem)
+library(broman)
 data(p713)
 
 y <- p713$counts[[1]]
@@ -24,5 +25,23 @@ if(file.exists(file)) {
 # find unique values
 
 # plot of log likelihood vs attempt
+pdf("../Figs/multiple_modes.pdf", height=5.5, width=10)
+par(mar=c(3.1,4.1,0.6,0.6))
+grayplot(ll, xlab="EM attempt", ylab="log likelihood",
+         mgp.x=c(1.6, 0.1, 0), mgp.y=c(3.1, 0.3, 0))
+dev.off()
+
 # table with estimates and log likelihood
+ll_round <- round(ll, 5)
+u_ll <- unique(ll_round)
+tab <- matrix(ncol=7+2, nrow=length(u_ll))
+colnames(tab) <- c(paste0("lambda_", c("0", "D", "B", "T")),
+                   "a", "b", "sig", "loglik", "n_hits")
+tab[,"loglik"] <- u_ll
+tab[,"n_hits"] <- sapply(u_ll, function(a) sum(ll_round==a))
+mle[match(u_ll, ll_round),]
+tab[,1:7] <- mle[match(u_ll, ll_round),]
+tab <- round(tab,2)
+tab <- tab[order(tab[,"loglik"], decreasing=TRUE), ]
+
 # proportion of starts that give the best value
