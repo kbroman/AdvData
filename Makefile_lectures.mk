93b2d01999441878627a6044c8ff1e522fb1932c
Karl Broman <kbroman@gmail.com>
2020-02-20 16:03:46 -0600

    Generic makefile for use with all lectures; ruby script to make notes
    - Starting work on making notes versions of lectures slides

---
+++ Makefile_lectures.mk
@@ -0,0 +1,23 @@
+# Common portion of the Makefile for each lecture
+
+WEBDIR=../docs
+R_OPTS=--no-save --no-restore --no-init-file --no-site-file
+
+web: $(WEBDIR)/$(LEC).pdf $(WEBDIR)/$(LEC)_notes.pdf
+
+$(WEBDIR)/%: %
+	cp $< $@
+
+$(LEC).pdf: $(LEC).tex ../LaTeX/header.tex $(FIGS)
+	xelatex $^
+
+$(LEC)_notes.pdf: $(LEC)_notes.tex ../LaTeX/header.tex $(FIGS)
+	xelatex $<
+	pdfnup $@ --nup 1x2 --no-landscape --paper letterpaper --frame true --scale 0.9
+	mv $(LEC)_notes-nup.pdf $@
+
+$(LEC)_notes.tex: $(LEC).tex ../Ruby/createVersionWithNotes.rb
+	../Ruby/createVersionWithNotes.rb $< $@
+
+clean:
+	rm *.aux *.log *.nav *.out *.snm *.toc *.vrb
