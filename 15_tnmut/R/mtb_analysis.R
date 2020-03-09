dffc0dea6bcab4701e52e4c493ea7a0f012713f9
Karl Broman <kbroman@gmail.com>
2020-03-09 16:44:08 -0500

    Lec 15: update mtb_analysis.R

--- 15_tnmut/R/mtb_analysis.R
+++ 15_tnmut/R/mtb_analysis.R
@@ -1,8 +1,8 @@
-attach("~/Projects/Gyanu/Nov02/R/.RData")
-mydata <- mydata
-taloc <- taloc
-finalres <- finalres
-detach(2)
+library(negenes)
+
+load("gyanu_results_nov02.RData")
+# contains mydata, taloc, finalres
+
 numTAs <- mydata[,1]+mydata[,3]+ c(0,mydata[-nrow(mydata),3])
 
 hit <- as.numeric(mydata[,2]>0 | mydata[,4]>0 |
@@ -11,7 +11,3 @@ hit <- as.numeric(mydata[,2]>0 | mydata[,4]>0 |
 out <- negenes(mydata[,1],mydata[,2],mydata[,3],mydata[,4],
                n.mcmc=1000,burnin=0,skip=0, ret=TRUE)
 mywh <- sort(sample((1:4204)[hit==0],50))
-
-
-
-
