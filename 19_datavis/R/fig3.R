9da16e170e20d2f4bca5871bdca1ce8eb7602d4c
Karl Broman <kbroman@gmail.com>
2020-03-23 10:19:11 -0500

    Lec 19: Revise "consider logs" fig

--- 19_datavis/R/fig3.R
+++ 19_datavis/R/fig3.R
@@ -9,13 +9,12 @@
 #save(x,y,z,file="fig3.RData",compress=TRUE)
 load("fig3.RData")
 
-bgcolor <- rgb(0.135, 0.135, 0.135, maxColorValue=1)
+bgcolor <- "white"
+fgcolor <- "black"
 
-#bitmap(file="../Figs/fig3a.bmp", width=6, height=5, res=288,
-#       pointsize=12)
 png(file="../Figs/fig3a.png", width=1200, height=1000, res=288,
        pointsize=12)
-par(las=1,fg="white",col="white",col.axis="white",col.lab="white",
+par(las=1,fg=fgcolor,col=fgcolor,col.axis=fgcolor,col.lab=fgcolor,
     bg=bgcolor,mar=c(5.1,4.1,0.6,0.6))
 plot(z,c(x,y),xlim=c(0.3,2.8),xaxt="n",xlab="Group",ylab="Response",
      lwd=2, log="y",yaxt="n")
@@ -28,11 +27,9 @@ abline(v=c(1,2),lty=2,col="gray70")
 dev.off()
 
 
-#bitmap(file="../Figs/fig3b.bmp", width=6, height=5, res=288,
-#       pointsize=12)
 png(file="../Figs/fig3b.png", width=1200, height=1000, res=288,
     pointsize=12)
-par(las=1,fg="white",col="white",col.axis="white",col.lab="white",
+par(las=1,fg=fgcolor,col=fgcolor,col.axis=fgcolor,col.lab=fgcolor,
     bg=bgcolor,mar=c(5.1,4.1,0.6,0.6))
 plot(z,c(x,y),xlim=c(0.3,2.8),xaxt="n",xlab="Group",ylab="Response",lwd=2)
 u <- par("usr")
@@ -40,5 +37,3 @@ segments(c(1,2),u[3],c(1,2),u[3]-diff(u[3:4])*0.02,xpd=TRUE)
 axis(side=1,at=c(1,2),labels=c("Treatment","Control"))
 abline(v=c(1,2),lty=2,col="gray70")
 dev.off()
-
-
