883c6333761e9f472a0296d19805ec878edbe1e0
Karl Broman <kbroman@gmail.com>
2020-04-06 22:39:25 -0500

    lec 21: small change to sel geno fig

--- 21_permtest/R/sel_geno.R
+++ 21_permtest/R/sel_geno.R
@@ -1,6 +1,7 @@
 library(qtl)
 
-png("../Figs/sel_geno.png", height=800, width=1600, pointsize=24)
+png("../Figs/sel_geno.png", height=800, width=1600, pointsize=32)
 data(hyper)
+par(mar=c(4.1,4.1,2.1,1.1))
 geno.image(hyper, reorder=1, ylab="Mice", main="")
 dev.off()
